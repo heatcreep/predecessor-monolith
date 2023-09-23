acae260c719471fb6197c80e2315da6f4e59a526
Andrew Owen <athomewithowen@duck.com>
2023-09-23 14:08:02 -0400

    add circular shape to profile avatars

--- app/src/main/java/com/aowen/monolith/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/aowen/monolith/ui/screens/profile/ProfileScreen.kt
@@ -1,6 +1,6 @@
 package com.aowen.monolith.ui.screens.profile
 
-import android.util.Log
+import android.content.res.Configuration
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -16,24 +16,26 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
 import coil.request.ImageRequest
 import com.aowen.monolith.FullScreenLoadingIndicator
+import com.aowen.monolith.R
+import com.aowen.monolith.data.UserInfo
 import com.aowen.monolith.navigation.navigateToLoginFromLogout
 import com.aowen.monolith.ui.theme.DiscordBlurple
 import com.aowen.monolith.ui.theme.DiscordDarkBackground
+import com.aowen.monolith.ui.theme.MonolithTheme
 import com.aowen.monolith.ui.theme.WarmWhite
 
 @Composable
@@ -78,91 +80,114 @@ fun ProfileScreen(
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 Surface(shape = RoundedCornerShape(5.dp)) {
-                    Column {
-                        if (uiState.userInfo != null) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(DiscordBlurple)
-                                    .padding(
-                                        24.dp
-                                    ),
-                                horizontalArrangement = Arrangement.SpaceBetween,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Column {
-                                    Text(
-                                        text = "signed in with Discord",
-                                        style = MaterialTheme.typography.titleSmall,
-                                        color = WarmWhite,
-                                    )
-                                    Text(
-                                        text = uiState.userInfo.fullName,
-                                        style = MaterialTheme.typography.titleLarge,
-                                        fontWeight = FontWeight.ExtraBold,
-                                        color = WarmWhite,
-                                    )
-                                }
-                                val model = ImageRequest.Builder(context)
-                                    .data(uiState.userInfo.avatarUrl)
-                                    .crossfade(true)
-                                    .build()
-                                SubcomposeAsyncImage(
-                                    modifier = Modifier.size(64.dp),
-                                    model = model,
-                                    contentDescription = null
-                                ) {
-                                    val state = painter.state
-                                    if (state is AsyncImagePainter.State.Success) {
-                                        SubcomposeAsyncImageContent()
-                                        LaunchedEffect(Unit) {
-                                            Log.d("ANDREWO", state.result.dataSource.toString())
-                                        }
-                                    }
-                                }
-                            }
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(DiscordDarkBackground)
-                                    .padding(24.dp),
-                                horizontalAlignment = Alignment.Start,
-                                verticalArrangement = Arrangement.spacedBy(16.dp)
-                            ) {
-                                Column {
-                                    Text(
-                                        text = "Email",
-                                        style = MaterialTheme.typography.bodySmall,
-                                        color = WarmWhite,
-                                    )
-                                    Text(
-                                        text = uiState.userInfo.email,
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = WarmWhite,
-                                    )
-                                }
-                            }
-                        } else {
+                    if (uiState.userInfo != null) {
+                        ProfileCard(userInfo = uiState.userInfo)
+                        TextButton(onClick = {
+                            onLogout()
+                            navigateToLogin()
+                            Toast.makeText(
+                                context,
+                                "logged out successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }) {
+                            Text(
+                                text = "Sign Out",
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = MaterialTheme.colorScheme.secondary
+                            )
+                        }
+                    } else {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp),
+                        ) {
                             Text(text = "Sorry, there was an error retrieving your profile. Please try again later.")
                         }
                     }
                 }
-                TextButton(onClick = {
-                    onLogout()
-                    navigateToLogin()
-                    Toast.makeText(
-                        context,
-                        "logged out successfully",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }) {
-                    Text(
-                        text = "Sign Out",
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = MaterialTheme.colorScheme.secondary
-                    )
-                }
+
             }
         }
+    }
+}
+
+@Composable
+fun ProfileCard(
+    userInfo: UserInfo
+) {
+
+    val context = LocalContext.current
+
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(DiscordBlurple)
+                .padding(
+                    24.dp
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text(
+                    text = "signed in with Discord",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = WarmWhite,
+                )
+                Text(
+                    text = userInfo.fullName,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = WarmWhite,
+                )
+            }
+            val model = ImageRequest.Builder(context)
+                .data(userInfo.avatarUrl)
+                .placeholder(R.drawable.unknown)
+                .crossfade(true)
+                .build()
+            SubcomposeAsyncImage(
+                modifier = Modifier.size(64.dp),
+                model = model,
+                contentDescription = null
+            ) {
+                SubcomposeAsyncImageContent()
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(DiscordDarkBackground)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Column {
+                Text(
+                    text = "Email",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = WarmWhite,
+                )
+                Text(
+                    text = userInfo.email,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = WarmWhite,
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun ProfileCardPreview() {
+    MonolithTheme {
+
     }
 }
