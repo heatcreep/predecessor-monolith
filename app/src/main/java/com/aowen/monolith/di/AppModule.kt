630955e113aa27ad88911f3edf1327d9f96bb4df
Andrew Owen <athomewithowen@duck.com>
2023-11-01 07:48:59 -0400

    add caching layer

--- app/src/main/java/com/aowen/monolith/di/AppModule.kt
+++ app/src/main/java/com/aowen/monolith/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.aowen.monolith.di
 
+import android.content.Context
 import com.aowen.monolith.BuildConfig
 import com.aowen.monolith.network.AuthRepository
 import com.aowen.monolith.network.AuthRepositoryImpl
@@ -11,9 +12,11 @@ import com.aowen.monolith.network.UserRecentSearchRepository
 import com.aowen.monolith.network.UserRecentSearchRepositoryImpl
 import com.aowen.monolith.network.UserRepository
 import com.aowen.monolith.network.UserRepositoryImpl
+import com.aowen.monolith.network.utils.NetworkUtil
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
@@ -21,6 +24,8 @@ import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.gotrue.gotrue
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.postgrest
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -31,10 +36,35 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(@ApplicationContext appContext: Context): Retrofit {
+
+        val cacheSize = 10 * 1024 * 1024L // 10 MB
+        val maxCacheLifeSize = 60 * 60 * 24 * 7 // 7 days
+        val maxAge = 5 // 5 seconds
+        val cache = Cache(appContext.cacheDir, cacheSize)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor { chain ->
+                var request = chain.request()
+                request = if (NetworkUtil.isNetworkAvailable(appContext))
+                    request.newBuilder().header(
+                        name = "Cache-Control",
+                        value = "public, max-age=$maxAge"
+                    ).build()
+                else
+                    request.newBuilder().header(
+                        "Cache-Control",
+                        "public, only-if-cached, max-stale=$maxCacheLifeSize"
+                    ).build()
+                chain.proceed(request)
+            }
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(RetrofitHelper.baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
--- app/src/main/java/com/aowen/monolith/network/utils/Utils.kt
+++ app/src/main/java/com/aowen/monolith/network/utils/Utils.kt
@@ -1,5 +1,31 @@
 package com.aowen.monolith.network.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
 fun String.trimExtraNewLine() = this
     .replace("\n\n", "")
     .replace("\r\n\r\n", "\n")
+
+object NetworkUtil {
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val nw      = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                //for check internet over Bluetooth
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            return connectivityManager.activeNetworkInfo?.isConnected ?: false
+        }
+    }
+}
