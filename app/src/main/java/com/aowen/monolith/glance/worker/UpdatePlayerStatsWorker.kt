185d60e1d270aa0b74048c6ff18a76e21d13c375
Andrew Owen <athomewithowen@duck.com>
2024-04-12 13:39:01 -0400

    fix issue that was crashing widget if user was on device lower than Android 12 "

--- app/src/main/java/com/aowen/monolith/glance/worker/UpdatePlayerStatsWorker.kt
+++ app/src/main/java/com/aowen/monolith/glance/worker/UpdatePlayerStatsWorker.kt
@@ -1,11 +1,16 @@
 package com.aowen.monolith.glance.worker
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
 import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.core.content.FileProvider.getUriForFile
 import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidgetManager
@@ -16,6 +21,7 @@ import androidx.work.CoroutineWorker
 import androidx.work.Data
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ExistingWorkPolicy
+import androidx.work.ForegroundInfo
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.PeriodicWorkRequest
@@ -48,6 +54,9 @@ class UpdatePlayerStatsWorker @AssistedInject constructor(
 
         private const val FORCE_KEY = "force"
         private const val REPEAT_INTERVAL_MINUTES = 15L
+
+        const val CHANNEL_ID = "ForegroundServiceChannel"
+        const val NOTIFICATION_ID = 1
 
         private val uniqueWorkName = UpdatePlayerStatsWorker::class.java.simpleName
 
@@ -154,6 +163,31 @@ class UpdatePlayerStatsWorker @AssistedInject constructor(
                 Result.failure()
             }
         }
+    }
+
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        createNotificationChannel()
+        val notification = createNotification()
+        return ForegroundInfo(NOTIFICATION_ID, notification)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = applicationContext.getSystemService(NotificationManager::class.java)
+            manager?.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    private fun createNotification(): Notification {
+        return NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setContentTitle("Monolith Player Stats")
+            .setContentText("Running in the background")
+            .build()
     }
 
     private suspend fun setWidgetState(newState: PlayerStatsState) {
